b16a113aba5e4bb9a91dcbbf46c115af1678a1ee
Simon Schulz <simon.schulz@active-group.de>
2017-12-18 13:22:20 +0100

    Free: fix code example

--- src/main/scala/database/AdressBookDatabase.scala
+++ src/main/scala/database/AdressBookDatabase.scala
@@ -1,13 +1,23 @@
 package database
 
 import domain.Address
-
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 object AddressBookDatabase {
   def put(address : Address) : Future[Either[Throwable, Unit]] = ???
   def get(id : Long) : Future[Either[Throwable, Option[Address]]] = ???
-  def delete(id : Address) : Future[Either[Throwable, Unit]] = ???
+  def delete(address : Address) : Future[Either[Throwable, Unit]] = ???
   def filter(foo : Address => Boolean) : Future[Either[Throwable, List[Address]]] = ???
+
+  def removeBielefeld : Future[Either[Throwable, List[Address]]] = {
+    val bielefeldFE = filter(_.town == "Bielefeld")
+    for {
+      addressesE <-bielefeldFE
+    } yield {
+      addressesE.map(_.foreach(address => delete(address)))
+      addressesE
+    }
+  }
 }
 
